51552a0890dc8ad91ba8755daa701207e430eaf0
Vladyslav <vurado3@gmail.com>
2017-02-15 12:19:09 +0300

    redirect

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
 from home import views
-from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    url(r'^home', views.index, name='index'),
-    url(r'^$', RedirectView.as_view(url='/home/')),
+    url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
 ]
