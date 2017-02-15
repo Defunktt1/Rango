0f7f4a9f4eec0d3a84b65285cd971ca4866ca959
Vladyslav <vurado3@gmail.com>
2017-02-15 12:18:53 +0300

    redirect

--- Rango/urls.py
+++ Rango/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 from home import views
 
 
 urlpatterns = [
-    url(r'^', include('home.urls')),
+    url(r'^$', RedirectView.as_view(url='/home/')),
     url(r'^home/', include('home.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
