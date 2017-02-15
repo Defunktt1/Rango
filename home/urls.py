c94dc38df2af6b8899f36300c892a4daff7c13a6
Vladyslav <vurado3@gmail.com>
2017-02-15 11:58:00 +0300

    redirect to /home page

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from home import views
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^home', views.index, name='index'),
+    url(r'^$', RedirectView.as_view(url='/home/')),
     url(r'^about/', views.about, name='about'),
 ]
