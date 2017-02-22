9568c89f6d92171b00bc2456a50b9a7d94973f9f
Vladyslav <vurado3@gmail.com>
2017-02-22 17:04:51 +0300

    Added 'add_page' form

--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from home.models import Category, Page
 from home.forms import CategoryForm, PageForm
+from django.template import RequestContext
 
 
 def index(request):
-    category_list = Category.objects.order_by('-likes')[:5]
+    category_list = Category.objects.order_by('-likes')[:10]
     top_pages = Page.objects.order_by('-views')[:5]
     context_dict = {"categories": category_list,
                     "pages": top_pages}
@@ -33,16 +34,15 @@ def show_category(request, category_name_slug):
 
 
 def add_category(request):
-    form = CategoryForm()
-
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CategoryForm(request.POST)
 
-        if form.is_valid():
+        if form.is_valid:
             form.save(commit=True)
-            return index(request)
         else:
             print(form.errors)
+    else:
+        form = CategoryForm()
     return render(request, 'home/add_category.html', {'form': form})
 
 
@@ -53,18 +53,18 @@ def add_page(request, category_name_slug):
         category = None
 
     form = PageForm()
+
     if request.method == 'POST':
         form = PageForm(request.POST)
-
         if form.is_valid():
             if category:
                 page = form.save(commit=False)
                 page.category = category
                 page.views = 0
                 page.save()
-                return show_category(request, category_name_slug)
+            return show_category(request, category_name_slug)
         else:
             print(form.errors)
 
-    context_dict = {'form': form, 'category':category}
-    return render(request, 'home/add_page.html', context_dict)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'home/add_page.html', context=context_dict)
